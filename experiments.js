c0ca53136b489e4806f4b1e70e67b31b6b673cac
True.for.fun <true.for.fun@gmail.com>
2023-01-26 20:33:12 +0400

    hardwork

--- experiments.js
+++ experiments.js
@@ -1,2 +1,7 @@
-
-console.log('OMGWTFBBQ');
+const timesToRepeat = 10
+const character =  `padla`
+let answer = ``
+for (let i = 0; i < timesToRepeat; i++) {
+  answer = answer + character
+}
+console.log (answer)
--- main.js
+++ main.js
@@ -1,3 +1,65 @@
 import experiments from './experiments.js'
 
 experiments()
+const monthlyrent = 500
+
+const yearlyrent = monthlyrent * 12
+
+console.log(yearlyrent);
+console.log("hello world")
+const myName = "Sergei Trifonov"
+console.log(myName)
+console.log(`don\'t do that\\
+
+
+pam
+`)
+const firstName = "Sergey"
+const surName = "Trifonov"
+const sentence = "Hello " + firstName + " " + surName + " ! How are you?"
+console.log(sentence)
+const sentencewithTamplate = `Hello ${firstName} ${surName}! how are you?`
+console.log(sentencewithTamplate)
+const skyIsBlue = true
+if (skyIsBlue) {
+  console.log(`the Sky is Blue!`)
+} else {
+  console.log (`the Sky is ... not blue`)
+}
+if (2+2===4) {
+  console.log (`you damn right`)
+}
+else  {
+  console.log (`no. but you can do this`) 
+}
+const friendsAtYourParty = 0
+if (friendsAtYourParty === 0) {
+  console.log (`cool, all whores are mine`)
+} else if (friendsAtYourParty <= 4) {
+  console.log (`ok, everyone can take two whores)`)
+} else {
+  console.log (`hey, whores for everyone`)
+}
+let friendsAtYourParty1 = 0
+friendsAtYourParty1 = friendsAtYourParty1 + 1
+friendsAtYourParty1 = friendsAtYourParty1 + 1
+friendsAtYourParty1 = friendsAtYourParty1 + 1
+friendsAtYourParty1 = friendsAtYourParty1 + 1
+console.log (friendsAtYourParty1)
+
+let girlsSuckingMyCock = 0
+while (girlsSuckingMyCock <10) {
+  console.log (`newOneStartsToSuck`)
+  girlsSuckingMyCock = girlsSuckingMyCock + 1
+}
+console.log (girlsSuckingMyCock)
+
+let YuliaSuckMyDick = 1
+YuliaSuckMyDick ++
+YuliaSuckMyDick += 1
+console.log (YuliaSuckMyDick)
+let TheDogBark = 0
+for (let i = 0; i < 10; i++) {
+  TheDogBark++
+}
+console.log (TheDogBark)
