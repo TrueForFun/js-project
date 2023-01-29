497dfec51d0ca5958edd4ee0f05d30b80d5e6112
True.for.fun <true.for.fun@gmail.com>
2023-01-29 20:53:17 +0400

    dom

--- experiments.js
+++ experiments.js
@@ -1,7 +1,114 @@
-const timesToRepeat = 10
-const character =  `padla`
-let answer = ``
-for (let i = 0; i < timesToRepeat; i++) {
-  answer = answer + character
+const timesToRepeat = 10;
+const samleString = `padla`;
+//let answer = ``
+//for (let i = 0; i < timesToRepeat; i++) {
+//answer = answer + samleString
+//}
+//console.log (answer)
+
+console.log("".padStart(timesToRepeat * samleString.length, samleString));
+
+function addTwo(number) {
+  return number + 2;
 }
-console.log (answer)
+const finalAnswer = addTwo(5);
+console.log(finalAnswer);
+
+let string = `Yulia is gonna give me a blowjob`;
+string = string.substring(9, 22);
+console.log(string);
+const sentence = `PaDla HuYadLa`;
+const lowercasesentence = sentence.toLowerCase();
+console.log(lowercasesentence);
+const number = 5.5;
+const roundedNumber = Math.round(number);
+console.log(roundedNumber);
+const testStringOne = `My dick is huge!`;
+const testStringTwo = `My dick is small!`;
+const testStringThree = `huge`;
+console.log(testStringOne.includes(testStringThree));
+console.log(testStringThree.includes(testStringTwo));
+console.log(Date.now());
+
+//the commons below is stricly recommended for the procedure//
+const person = {
+  name: `Sergei the Great`,
+  city: `kobulety`,
+  state: `Georgia`,
+  favoriteOccupation: `having sex`,
+  wannaSexNow: true,
+  numberOfSex: 100,
+};
+console.log(person);
+console.log(person.name);
+
+const person1 = {
+  name: `Anna`,
+  ageRange: `20-25`,
+};
+
+const person2 = {
+  name: `Babulya`,
+  ageRange: `55-60`,
+};
+
+function suggestTheMovie(person) {
+  if (person.ageRange === `20-25`) {
+    console.log(`I think Muyagi will suits you`);
+  } else if (person.ageRange === `55-60`) {
+    console.log(`The Homecoming will do`);
+  } else {
+    console.log(`no one gives a fuck about your dreams, Sergei`);
+  }
+}
+
+suggestTheMovie(person1);
+
+const Julia = {
+  speak() {
+    console.log(`woof-woof`);
+  },
+};
+Julia.speak();
+
+const me = {
+  name: {
+    first: `Sergei`,
+    last: `Trifonov`,
+  },
+  location: {
+    street: `sahara`,
+    city: `kobuleti`,
+    state: `georgia`,
+  },
+  getAddress() {
+    return `${this.name.first} ${this.name.last} ${this.location.street} ${this.location.city} ${this.location.state}`;
+  },
+};
+
+console.log(me.getAddress());
+
+const primeNumbers = [1, 3, 5, 7, 8, 9];
+console.log(primeNumbers.length);
+console.log(primeNumbers.join(`!!!`));
+
+const cities = [`Razuvaevka`, `Moscow`, `New-York`, `Amsterdam`, `Phuket`];
+
+for (let i = 0; i < cities.length; i++) {
+  console.log(cities[i]);
+  console.log(i, cities[i]);
+  // this time i shows only the number of cities,
+  // where cities[i] shows us the cities itself ( city + 1,+1.. according to the function)
+  // i + cities[i] give us both variants
+}
+
+function logCity(city) {
+  console.log(city);
+}
+cities.forEach(logCity);
+
+const elementsToChange = document.querySelectorAll(".js-target");
+for (let i = 0; i < elementsToChange.length; i++) {
+  const currentElement = elementsToChange[i];
+  currentElement.innerText = "Modified by JavaScript!";
+}
--- main.js
+++ main.js
@@ -1,65 +1,83 @@
-import experiments from './experiments.js'
+import experiments from "./experiments.js";
 
-experiments()
-const monthlyrent = 500
+experiments();
+const monthlyrent = 500;
 
-const yearlyrent = monthlyrent * 12
+const yearlyrent = monthlyrent * 12;
 
 console.log(yearlyrent);
-console.log("hello world")
-const myName = "Sergei Trifonov"
-console.log(myName)
+console.log("hello world");
+const myName = "Sergei Trifonov";
+console.log(myName);
 console.log(`don\'t do that\\
 
 
 pam
-`)
-const firstName = "Sergey"
-const surName = "Trifonov"
-const sentence = "Hello " + firstName + " " + surName + " ! How are you?"
-console.log(sentence)
-const sentencewithTamplate = `Hello ${firstName} ${surName}! how are you?`
-console.log(sentencewithTamplate)
-const skyIsBlue = true
+`);
+const firstName = "Sergey";
+const surName = "Trifonov";
+const sentence = "Hello " + firstName + " " + surName + " ! How are you?";
+console.log(sentence);
+const sentencewithTamplate = `Hello ${firstName} ${surName}! how are you?`;
+console.log(sentencewithTamplate);
+const skyIsBlue = true;
 if (skyIsBlue) {
-  console.log(`the Sky is Blue!`)
+  console.log(`the Sky is Blue!`);
 } else {
-  console.log (`the Sky is ... not blue`)
+  console.log(`the Sky is ... not blue`);
 }
-if (2+2===4) {
-  console.log (`you damn right`)
+if (2 + 2 === 4) {
+  console.log(`you damn right`);
+} else {
+  console.log(`no. but you can do this`);
 }
-else  {
-  console.log (`no. but you can do this`) 
-}
-const friendsAtYourParty = 0
+const friendsAtYourParty = 0;
 if (friendsAtYourParty === 0) {
-  console.log (`cool, all whores are mine`)
+  console.log(`cool, all whores are mine`);
 } else if (friendsAtYourParty <= 4) {
-  console.log (`ok, everyone can take two whores)`)
+  console.log(`ok, everyone can take two whores)`);
 } else {
-  console.log (`hey, whores for everyone`)
+  console.log(`hey, whores for everyone`);
 }
-let friendsAtYourParty1 = 0
-friendsAtYourParty1 = friendsAtYourParty1 + 1
-friendsAtYourParty1 = friendsAtYourParty1 + 1
-friendsAtYourParty1 = friendsAtYourParty1 + 1
-friendsAtYourParty1 = friendsAtYourParty1 + 1
-console.log (friendsAtYourParty1)
+let friendsAtYourParty1 = 0;
+friendsAtYourParty1 = friendsAtYourParty1 + 1;
+friendsAtYourParty1 = friendsAtYourParty1 + 1;
+friendsAtYourParty1 = friendsAtYourParty1 + 1;
+friendsAtYourParty1 = friendsAtYourParty1 + 1;
+console.log(friendsAtYourParty1);
 
-let girlsSuckingMyCock = 0
-while (girlsSuckingMyCock <10) {
-  console.log (`newOneStartsToSuck`)
-  girlsSuckingMyCock = girlsSuckingMyCock + 1
+let girlsSuckingMyCock = 0;
+while (girlsSuckingMyCock < 10) {
+  console.log(`newOneStartsToSuck`);
+  girlsSuckingMyCock = girlsSuckingMyCock + 1;
 }
-console.log (girlsSuckingMyCock)
+console.log(girlsSuckingMyCock);
 
-let YuliaSuckMyDick = 1
-YuliaSuckMyDick ++
-YuliaSuckMyDick += 1
-console.log (YuliaSuckMyDick)
-let TheDogBark = 0
+let YuliaSuckMyDick = 1;
+YuliaSuckMyDick++;
+YuliaSuckMyDick += 1;
+console.log(YuliaSuckMyDick);
+let TheDogBark = 0;
 for (let i = 0; i < 10; i++) {
-  TheDogBark++
+  TheDogBark++;
 }
-console.log (TheDogBark)
+console.log(TheDogBark);
+
+const person1 = {
+  name: `Anna`,
+  ageRange: `20-25`,
+};
+const person2 = {
+  name: `Babulya`,
+  ageRange: `55-60`,
+};
+function suggestTheMovie(person) {
+  if (person.ageRange === `20-25`) {
+    console.log(`I think Muyagi will suits you`);
+  } else if (person.ageRange === `55-60`) {
+    console.log(`The Homecoming will do`);
+  } else {
+    console.log(`no one gives a fuck about your dreams, Sergei`);
+  }
+}
+suggestTheMovie(person1);
